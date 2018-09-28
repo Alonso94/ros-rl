473c6bc6501593d0152806980cd5f760847f4f6a
ermekaitygulov <ermek.imankulov@gmail.com>
2018-09-28 12:23:33 +0300

    added work with img

--- TRPOagent/agent/agent_img.py
+++ TRPOagent/agent/agent_img.py
@@ -1,6 +1,7 @@
 from util import *
 import numpy as np
 from network.network_img import NetworkImg
+import cv2
 import rospy
 from agent.agent_TRPO import TRPOAgent
 
@@ -25,7 +26,7 @@ class AgentImg(TRPOAgent):
         self.env.state = np.zeros(4)
         while total_timesteps < n_timesteps:
             observations, actions, rewards, action_m, action_logstd = [], [], [], [], []
-            observation = self.env.reset()
+            observation = self.proc_img(self.env.reset())
             if total_timesteps % 4 == 0:
                 self.env.state = np.zeros(4)
             done = False
@@ -38,6 +39,7 @@ class AgentImg(TRPOAgent):
                 reward = 0
                 for i in range(self.n_actions):
                     observation, r, done = self.env.step([i, action[i]])
+                    observation = self.proc_img(observation)
                     reward += r
                 rewards.append(reward)
                 total_timesteps += 1
@@ -52,3 +54,34 @@ class AgentImg(TRPOAgent):
                     paths.append(path)
                     break
         return paths
+
+    def proc_img(self, img):
+        result = img.T
+        gray = cv2.cvtColor(img.T, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        # noise removal
+        kernel = np.ones((3, 3), np.uint8)
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+        # sure background area
+        sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+        # Finding sure foreground area
+        dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+        ret, sure_fg = cv2.threshold(dist_transform, 0.01 * dist_transform.max(), 255, 0)
+
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg, sure_fg)
+
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(sure_fg)
+
+        # Add one to all labels so that sure background is not 0, but 1
+        markers = markers + 1
+
+        # Now, mark the region of unknown with zero
+        markers[unknown == 255] = 0
+        result[markers == -1] = [255, 0, 0]
+        result[:,:,1] = 50*markers
+        return result
--- TRPOagent/network/network_img.py
+++ TRPOagent/network/network_img.py
@@ -2,7 +2,15 @@ from lasagne.layers import *
 from lasagne.nonlinearities import linear
 from lasagne.init import GlorotNormal
 from network.network import Network
+import theano.tensor as T
 
 class NetworkImg(Network):
     def init_model(self, n_actions, observation_shape):
-
+        self.observations=T.tensor4("X")
+        nn = InputLayer((None,) + observation_shape, input_var=self.observations)
+        conv1 = Conv2DLayer(nn, 8, 3, 1)
+        #conv2 = Conv2DLayer(conv1, 32, 3, 1)
+        nn1 = DenseLayer(conv1, 8)
+        self.m = DenseLayer(nn1, n_actions, nonlinearity=linear)
+        self.logsigma = DenseLayer(nn1, n_actions, nonlinearity=linear)
+        self.model = [self.m, self.logsigma]
